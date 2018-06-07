7328601f1973e3486cbc56c54dddeccfcdf39094
Stephen J Naughton <steve@notaclue.net>
2018-06-07 17:53:15 +0100

    Working on a sort of DataGrid

---
+++ NotAClue.BlazorComponents/ExampleJsInterop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Blazor.Browser.Interop;
+
+namespace NotAClue.BlazorComponents
+{
+    public class ExampleJsInterop
+    {
+        public static string Prompt(string message)
+        {
+            return RegisteredFunction.Invoke<string>(
+                "NotAClue.BlazorComponents.ExampleJsInterop.Prompt",
+                message);
+        }
+    }
+}
